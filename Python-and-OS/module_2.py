74d4f1bbd6dd60152b5e7649cdb282dd9256ac64
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2024-12-11 20:50:23 +0800

    added new notes

--- Python-and-OS/module_2.py
+++ Python-and-OS/module_2.py
@@ -1,2 +1,6 @@
 #Code Reuse
 print("Hello World!")
+
+#Every python program, or automation program needs to have a __init__.py file
+#This file is used to initialize the module, and for the interpreter to know 
+#that this is a module
