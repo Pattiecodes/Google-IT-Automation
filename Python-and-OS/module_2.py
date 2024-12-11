a42757db35868b1cc21a6e04578411a620732fb0
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2024-12-11 21:01:43 +0800

    added more on module_2.py notes.

--- Python-and-OS/module_2.py
+++ Python-and-OS/module_2.py
@@ -4,3 +4,8 @@ print("Hello World!")
 #Every python program, or automation program needs to have a __init__.py file
 #This file is used to initialize the module, and for the interpreter to know 
 #that this is a module
+
+#It is important that as IT Specialists, we should know how to use different IDEs,
+# and Code Editors. This is because we will be working with different teams.
+# Code Editors such as Atom, Notepad++, Sublime Text, Eclipse, and Pycharm.
+# CLIs such as Vim, Emacs, and Nano.
