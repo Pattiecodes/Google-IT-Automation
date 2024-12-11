0169772dda2f29f98959e06a5f620920d74c44da
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2024-12-11 20:56:39 +0800

    added donut function in the areas.py module

--- Python-and-OS/areas.py
+++ Python-and-OS/areas.py
@@ -8,3 +8,8 @@ def rectangle(base, height):
 
 def circle(radius):
     return math.pi * (radius ** 2)
+
+#Donut function is used to demonstrate using the Atom Code Editor, 
+# integrated in the Atom IDE in a linux machine.
+def donut(outside_radius, inside_radius):
+    return circle(outside_radius) - circle(inside_radius)
