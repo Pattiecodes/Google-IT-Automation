9d38caf2e8b016aa5e36bfb8b070a090a6c8105f
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2024-12-16 19:40:06 +0800

    added directories section in working_with_files.py

--- Python-and-OS/working_with_files.py
+++ Python-and-OS/working_with_files.py
@@ -11,3 +11,11 @@ os.rename("first_draft.txt", "finished_masterpiece.txt")
 #Checks if file exists
 os.path.exists("finished_masterpiece.txt")
 os.path.exists("userlist.txt") 
+
+"""Directories"""
+
+print(os.getcwd())
+#This code snippet returns the current working directory.
+
+os.mkdir("new_dir")
+#The os.mkdir("new_dir") function creates a new directory called new_dir
