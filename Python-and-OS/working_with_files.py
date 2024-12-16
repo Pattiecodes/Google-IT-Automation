9fa7c6c8181b29807ff6306161709a4057e849fa
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2024-12-16 19:29:52 +0800

    fixed working_with_files.py

--- Python-and-OS/working_with_files.py
+++ Python-and-OS/working_with_files.py
@@ -1,11 +1,13 @@
 # Removes novel.txt
 import os
 os.remove("novel.txt")
+
+#Removing it again will throw an error. As file does not exist.
 os.remove("novel.txt")
 
-#Throws an error as this file does not exist.
+#Renaming a file. 
 os.rename("first_draft.txt", "finished_masterpiece.txt")
 
-#Renaming a file. 
+#Checks if file exists
 os.path.exists("finished_masterpiece.txt")
-os.path.exists("userlist.txt")
+os.path.exists("userlist.txt") 
