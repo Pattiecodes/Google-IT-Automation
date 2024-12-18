e3dad757b3cf4232f0408e93ce2b0bc34bc5df56
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2024-12-18 14:51:18 +0800

    added RegEx in Action

--- Python-and-OS/notesm3_programs.py
+++ Python-and-OS/notesm3_programs.py
@@ -73,3 +73,26 @@ print(re.search(r"o+l+", "boil"))
 import re
 print(re.search(r"p?each", "To each their own"))
 print(re.search(r"p?each", "I like peaches"))
+
+#Escaping Characters
+import re
+print(re.search(r".com", "welcome"))
+print(re.search(r"\.com", "welcome"))
+print(re.search(r"\.com", "mydomain.com"))
+
+import re
+print(re.search(r"\w*", "This is an example"))
+print(re.search(r"\w*", "And_this_is_another"))
+
+#Regular Expressions in Action
+import re
+print(re.search(r"A.*a", "Argentina"))
+print(re.search(r"A.*a", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Australia"))
+
+import re
+pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
+print(re.search(pattern, "_this_is_a_valid_variable_name"))
+print(re.search(pattern, "this isn't a valid variable"))
+print(re.search(pattern, "my_variable1"))
+print(re.search(pattern, "2my_variable1"))
